ac1e314cb7befcecb321863ba5803c7a1bfaa9c9
Waliston <goneisgone@hotmail.com>
2023-07-01 10:10:02 -0300

    Final Project UPDATE

--- Insert/getAllData.js
+++ Insert/getAllData.js
@@ -9,7 +9,6 @@ const options = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYjkyZDJkNzAwMGU1N2EyNzk1ZDgyMzZmYzMyMGViOSIsInN1YiI6IjY0OWY1NWI1ODFkYTM5MDBlYTdiOWEzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fECK1TJtzJ0PMRYjYzh88u_J5I_BhchgfMNt_am0a4A",
   },
 };
-
 const getMoviesData = async () => {
   const url =
     "https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=en-US&page=1&primary_release_year=1990&sort_by=popularity.desc";
