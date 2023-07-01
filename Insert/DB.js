940b721317d9a243a266021bee40b7a04fc2eaf4
Waliston <goneisgone@hotmail.com>
2023-06-30 22:45:55 -0300

    Final Project UPDATE

---
+++ Insert/DB.js
@@ -0,0 +1,47 @@
+const mysql = require("mysql");
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "40028922"
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+  con.query("CREATE DATABASE filmes");
+
+  con.query("USE filmes");
+});
+
+setTimeout(() => {
+  const diretor_table =
+    "CREATE TABLE diretor (idDiretor INT NOT NULL PRIMARY KEY,nome VARCHAR(45), pais VARCHAR(255));";
+  const ator_table =
+    "CREATE TABLE ator (idAtor INT NOT NULL PRIMARY KEY, nome VARCHAR(255),sexo VARCHAR(1), nasc DATE , pais VARCHAR(255), altura DECIMAL(3,2))";
+  const filme_table =
+    "CREATE  TABLE filme ( idFilme INT NOT NULL PRIMARY KEY, idDiretor INT NOT NULL , titulo VARCHAR(45) NOT NULL , ano INT NOT NULL , classificacao VARCHAR(45) NOT NULL, nota DOUBLE NOT NULL, bilheteria DOUBLE NOT NULL , custo DOUBLE NOT NULL , idFilmeAnterior INT, INDEX fk_filme_diretor1_idx (idDiretor ASC) , CONSTRAINT fk_filme_diretor1 FOREIGN KEY (idDiretor ) REFERENCES diretor (idDiretor ), CONSTRAINT fk_filme_filme1 FOREIGN KEY (idFilmeAnterior ) REFERENCES filme (idFilme ));";
+  const elenco_table =
+    "CREATE  TABLE `elenco` ( `idFilme` INT NOT NULL , `idAtor` INT NOT NULL , PRIMARY KEY (`idFilme`, `idAtor`) , INDEX `fk_elenco_filme_idx` (`idFilme` ASC) , INDEX `fk_elenco_ator_idx` (`idAtor` ASC) , CONSTRAINT `fk_elenco_filme` FOREIGN KEY (`idFilme`) REFERENCES `filme` (`idFilme` ), CONSTRAINT `fk_elenco_ator` FOREIGN KEY (`idAtor` ) REFERENCES `ator` (`idAtor` ));";
+
+  con.query(diretor_table, function (err, result) {
+    if (err) throw err;
+    console.log("Diretor Table created");
+  });
+
+  con.query(ator_table, function (err, result) {
+    if (err) throw err;
+    console.log("Ator Table created");
+  });
+
+  con.query(filme_table, function (err, result) {
+    if (err) throw err;
+    console.log("Filme Table created");
+  });
+
+  con.query(elenco_table, function (err, result) {
+    if (err) throw err;
+    console.log("Elenco Table created");
+  });
+}, 3000);
--- Insert/Project.js
+++ Insert/Project.js
@@ -1,41 +1,9 @@
-const axios = require("axios");
-const fs = require("fs");
+const getAllData = require("./getAllData")
 
-const getAllData = async () => {
-  const url =
-    "https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=en-US&page=1&primary_release_year=1990&sort_by=popularity.desc";
+//Get All data end save in a JSON file
+getAllData.getAllData();
 
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYjkyZDJkNzAwMGU1N2EyNzk1ZDgyMzZmYzMyMGViOSIsInN1YiI6IjY0OWY1NWI1ODFkYTM5MDBlYTdiOWEzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fECK1TJtzJ0PMRYjYzh88u_J5I_BhchgfMNt_am0a4A",
-    },
-  };
 
-  axios
-    .get(url, options)
-    .then((result) => {
-      console.log(result.data);
+//DB Connection
+require("./DB.js");
 
-      var data = {};
-      data.table = [];
-      for (i = 0; i < 100; i++) {
-        var obj = {
-          info: result.data,
-        };
-
-        data.table.push(obj);
-      }
-      fs.writeFile("input.json", JSON.stringify(data), function (err) {
-        if (err) throw err;
-        console.log("complete");
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
-
-getAllData();
