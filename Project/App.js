d44c1aba3e78a62276e13d1dd49213fefba95dee
Waliston <goneisgone@hotmail.com>
2023-07-01 17:46:28 -0300

    Final Project UPDATE

--- Project/App.js
+++ Project/App.js
@@ -1,5 +1,9 @@
 //DB Connection
-import "./DB.js";
+require("./DB.js");
+
+const  {  getMoviesData,
+    getAtorData,
+    getElencoData} = require("./getData.js") 
 
 //Get All data end save in a JSON file
 getAtorData();
