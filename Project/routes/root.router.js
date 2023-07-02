a32f6b21f6e559ef106767612380978aba3891e7
Waliston <goneisgone@hotmail.com>
2023-07-02 18:59:10 -0300

    DB Project -Master  Update

---
+++ Project/routes/root.router.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const rootRouter = express.Router();
+
+rootRouter.get("/", async (req, res) => {
+  try {
+    res.status(200).render(path.join(__dirname, "..", "public", "home.html"));
+  } catch (error) {
+    console.log({ message: error });
+  }
+});
+
+module.exports = rootRouter;
