55c159b1591f7fa74f339c4332d6fd7d286ac537
Waliston <goneisgone@hotmail.com>
2023-07-01 22:13:16 -0300

    Final Project UPDATE

--- Project/DB.js
+++ Project/DB.js
@@ -25,6 +25,8 @@ setTimeout(() => {
   const filme_table =
     "CREATE  TABLE filme ( idFilme INT  PRIMARY KEY AUTO_INCREMENT, idDiretor INT  , titulo VARCHAR(45)  , ano VARCHAR(45)  , classificacao VARCHAR(45) , nota DOUBLE , bilheteria DOUBLE  , custo VARCHAR(45)  , idFilmeAnterior INT, INDEX fk_filme_diretor1_idx (idDiretor ASC) , CONSTRAINT fk_filme_diretor1 FOREIGN KEY (idDiretor ) REFERENCES diretor (idDiretor ), CONSTRAINT fk_filme_filme1 FOREIGN KEY (idFilmeAnterior ) REFERENCES filme (idFilme ));";
 
+  const elenco_table = "CREATE  TABLE elenco ( idFilme INT NOT NULL , idAtor INT NOT NULL , PRIMARY KEY (idFilme, idAtor) , INDEX fk_elenco_filme_idx (idFilme ASC) ,INDEX fk_elenco_ator_idx (idAtor ASC) , CONSTRAINT fk_elenco_filme FOREIGN KEY (idFilme ) REFERENCES filme (idFilme ), CONSTRAINT fk_elenco_ator FOREIGN KEY (idAtor ) REFERENCES ator (idAtor ));"
+
   con.query(diretor_table, function (err, result) {
     if (err) throw err;
     console.log("Diretor Table created");
@@ -38,6 +40,11 @@ setTimeout(() => {
   con.query(filme_table, function (err, result) {
     if (err) throw err;
     console.log("Filme Table created");
+  });
+
+  con.query(elenco_table, function (err, result) {
+    if (err) throw err;
+    console.log("Elenco Table created");
   });
 }, 1000);
 
