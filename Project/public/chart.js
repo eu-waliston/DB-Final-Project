0e7bfbb4aeecdff5b0dda7b17a335dbcddc62315
LAB LAMOT <lab.lamot@gmail.com>
2023-07-03 17:12:16 -0300

    Update

--- Project/public/chart.js
+++ Project/public/chart.js
@@ -1,6 +1,9 @@
-const ctx = document.getElementById("myChart");
+const ctx1 = document.getElementById("myChart");
+const ctx2 = document.getElementById("myChart2");
+const ctx3 = document.getElementById("myChart3");
+const ctx4 = document.getElementById("myChart4");
 
-const chart1 = new Chart(ctx, {
+const graphChart = new Chart(ctx1, {
   type: "bar",
   data: {
     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
@@ -20,3 +23,58 @@ const chart1 = new Chart(ctx, {
     },
   },
 });
+
+const mixedChart = new Chart(ctx2, {
+  type: 'polarArea',
+  data: {
+
+    datasets: [{
+      label: 'Atores por pais',
+      data: [11, 16, 7, 3, 14],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(75, 192, 192)',
+        'rgb(255, 205, 86)',
+        'rgb(201, 203, 207)',
+        'rgb(54, 162, 235)'
+      ]
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+const pieChart = new Chart(ctx3, {
+  type: 'pie',
+  data:  {
+    labels: [
+      'Red',
+      'Blue',
+      'Yellow'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  },
+
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+
+})
