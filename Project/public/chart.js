5b0beb590ef55d627e367e96f0d349d565f10e81
Waliston <goneisgone@hotmail.com>
2023-07-03 19:45:42 -0300

    DB Project - Update 🚀 - Functions

--- Project/public/chart.js
+++ Project/public/chart.js
@@ -23,26 +23,22 @@ const graphChart = new Chart(ctx1, {
   },
 });
 
-
-
 const pieChart = new Chart(ctx3, {
-  type: 'pie',
-  data:  {
-    labels: [
-      'Red',
-      'Blue',
-      'Yellow'
+  type: "pie",
+  data: {
+    labels: ["Red", "Blue", "Yellow"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [300, 50, 100],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+        hoverOffset: 4,
+      },
     ],
-    datasets: [{
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-      ],
-      hoverOffset: 4
-    }]
   },
 
   options: {
@@ -52,5 +48,44 @@ const pieChart = new Chart(ctx3, {
       },
     },
   },
+});
 
-})
+const lineChart = new Chart(ctx2, {
+  type: "bar",
+  data: (data = {
+    labels: "Days",
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(201, 203, 207, 0.2)",
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  }),
+
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
